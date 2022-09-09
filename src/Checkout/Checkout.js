39af390423693cdc0610b2de92e3c33ff725c26a
Seyit <seyityuce8@gmail.com>
2022-09-09 12:28:32 +0300

    head title and favicon updated

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -13,7 +13,7 @@ function Checkout() {
       <div className='checkout-left'>
         <img className='checkout-ad' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
         <div>
-          {/* <h3> Hello, {user.id} </h3> */}
+          {/* <h3> Hello, {user?.email} </h3> */}
           <h2 className='checkout-title'>
             Your Shopping Basket
           </h2>
